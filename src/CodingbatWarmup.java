d52a2e366c07b77f6b48389bd58b49b10e638820
Jose Ortiz <jose.ortiz@live.com>
2020-10-01 21:59:03 -0600

    Added 21th problem. Need to find alt code. Need to add test.

--- src/CodingbatWarmup.java
+++ src/CodingbatWarmup.java
@@ -119,7 +119,11 @@ public class CodingbatWarmup {
     return found == 1;
   }
 
-
+  public String delDel(String str) {
+    if( str.length() > 3 && str.charAt(1) == 'd' && str.charAt(2) == 'e'&& str.charAt(3) == 'l' )
+      return str.substring(0,1) + str.substring(4,str.length());
+    else return str;
+  }
 
 
 }
